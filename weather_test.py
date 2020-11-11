eb0c25e5d1f22af0b11e96175bca9fdfaadb91ce
Your name here <akorott@gmail.com>
2020-11-11 11:52:22 -0300

    Initial commit

---
+++ weather_test.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+# beautiful soup is an HTML/CSS parser
+
+target_city = 'Toronto'
+target_month = 'January'
+
+URL = f'https://weather-and-climate.com/{target_city}-{target_month}-averages'
+
+soup = BeautifulSoup(urlopen(URL), 'lxml')
+
+min_temp = soup.find('span', class_='label', text='Min Temperature').find_next('span').text
+max_temp = soup.find('span', class_='label', text='Max Temperature').find_next('span').text
+average_precipitation = soup.find('span', class_='label', text='Precipitation').find_next('span').text
+chance_of_precipitation = soup.find('span', class_='label', text='Chance of Rain').find_next('span').text
+
+def convert_to_fahrenheit(temp):
+    celcius = int(temp.split()[0])
+    fahrenheit = round(celcius * (9/5) + 32)
+    return fahrenheit
+
+def forecast_details(city,month):
+
+    return f'\nCity: {city}  \nMonth: {month}  \nMin Temp: {min_temp} / {convert_to_fahrenheit(min_temp)} °F  \nMax Temp: {max_temp} / {convert_to_fahrenheit(max_temp)} °F  \nAvg Monthly Precipitation: {average_precipitation}  \nDaily Chance of Precipitation: {chance_of_precipitation}'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# print(target_city[0])
+
+# for i in range(len(target_city)):
+#     forecast_details(target_city[i],target_month)
+
+# def forecast_details(city,month):
+#     print(f'City: {city}')
+#     print(f'Month: {month}')
+#     print(f'Minimum Temperature: {min_temp} / {convert_to_fahrenheit(min_temp)} °F')
+#     print(f'Maximum Temperature: {max_temp} / {convert_to_fahrenheit(max_temp)} °F')
+#     print(f'Average Monthly Precipitation: {average_precipitation}')
+#     print(f'Daily Chance of Precipitation: {chance_of_precipitation}')
+
+
